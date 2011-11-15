12fb961dc00051fecebaee6173fd94492685d21d
tbranyen <tim@tabdeveloper.com>
2011-11-15 00:36:39 -0500

    General code cleanup, less messy

--- backbone.layoutmanager.js
+++ backbone.layoutmanager.js
@@ -9,7 +9,7 @@
 
 // LayoutManager at its core is specifically a Backbone.View
 Backbone.LayoutManager = Backbone.View.extend({
-  
+
   initialize: function(opts) {
     // Handle partials support
     var partials = {};
@@ -40,123 +40,152 @@ Backbone.LayoutManager = Backbone.View.extend({
   },
 
   render: function(done) {
-    var layout, handler, prefix;
+    var contents, prefix;
     var manager = this;
     var options = this.options;
 
-    function cont(_layout) {
-      // Inject into el
-      layout = manager.el;
-      layout.innerHTML = _layout;
+    // Cache templates into manager.cache
+    // @path     : View's template property.
+    // @contents : Template content's to cache.
+    function cache(path, contents) {
+      if (manager.cache[path]) {
+        return manager.cache[path];
+      }
+
+      return manager.cache[path] = contents;
+    }
+
+    // Returns an object that provides asynchronous capabilities.
+    function async(done) {
+      return {
+        async: function() {
+          return done;
+        }
+      };
+    }
+
+    // Passed to each View's render.  This function handles the wrapped
+    // View and the call to render.
+    function viewRender(view) {
+      // Once the template is successfully fetched, use its contents to
+      // proceed.
+      function templateDone(contents, context) {
+        // Render the partial into the View's el property.
+        view.el.innerHTML = options.render.call(options, contents, context);
+
+        // Signal that the fetching is done, wrap in a setTimeout to ensure,
+        // that synchronous calls do not break the done being triggered.
+        window.setTimeout(function() {
+          handler.done(view.el)
+        }, 0);
+      }
+
+      // Create an asynchronous handler.
+      var handler = async(templateDone);
+
+      // Return the render method for View's to call.
+      return {
+        // Render accepts an option context object.
+        render: function(context) {
+          // Seek out serialize method and use that object.
+          if (!context && _.isFunction(view.serialize)) {
+            context = view.serialize.call(view);
+          }
+
+          // Set the prefix
+          prefix = options.paths && options.paths.template || "";
+
+          // Fetch layout and template contents
+          contents = options.fetch.call(handler, prefix + view.template);
+
+          // If the function was synchronous, continue execution.
+          if (contents) {
+            templateDone(contents, context);
+          }
+
+          return handler;
+        }
+      };
+    }
+
+    // Once the layout is successfully fetched, use its contents to proceed.
+    function layoutDone(contents) {
+      // Set the contents of the layout element.
+      manager.el.innerHTML = contents;
 
       // Iterate over each partial and apply the render method
       _.each(manager.partials, function(view, name) {
         // Render into a variable
-        view.render(layoutCallback).done = function(contents) {
+        view.render(viewRender).done = function(contents) {
           // Apply partially
-          options.partial(layout, name, contents);
+          options.partial(manager.el, name, contents);
         };
       });
 
-      done(layout);
+      // Call the original LayoutManager render method callback, with the
+      // DOM element containing the layout and sub views.
+      done(manager.el);
     }
 
-    // Passed to view render methods
-    function layoutCallback(view) {
-      var _context = {};
-
-      function render(context) {
-        var contents, template;
-
-        var cont = function(_contents) {
-          contents = _contents;
-
-          // Render the partial
-          view.el.innerHTML = options.render.call(options, contents, _context);
-
-          // Signal that the fetching is done
-          handler.done(view.el);
-        };
-
-        // Use to allow for methods to become async
-        var handler = {
-          async: function() {
-            return cont;
-          },
-
-          done: $.noop
-        };
-
-        // Seek out serialize method and extend
-        if (!context && _.isFunction(view.serialize)) {
-          _.extend(_context, view.serialize.call(view));
-
-        // Extend passed context object
-        } else if (_.isObject(context)) {
-          _.extend(_context, context);
-        }
-
-        // Set the prefix
-        prefix = options.paths && options.paths.template || "";
-
-        // Fetch layout and template contents
-        if (contents = options.fetch.call(handler, prefix + view.template)) {
-          cont(contents);
-        }
-
-        return handler;
-      }
-
-      return { render: render };
-    }
-
-    // Use to allow for methods to become async
-    handler = {
-      async: function() {
-        return cont;
-      }
-    };
-
+    // This is essentially the pathing prefix.
     prefix = options.paths && options.paths.layout || "";
 
     // Get layout contents
-    if (layout = options.fetch.call(handler, prefix + options.name)) {
-      return cont(layout);
+    contents = options.fetch.call(async(layoutDone), prefix + options.name);
+
+    // If the function was synchronous, continue execution.
+    if (contents) {
+      return layoutDone(contents);
     }
   }
 
 },
-
-// Constructor properties
 {
-  // Set global configuration options
+  // This static method allows for global configuration of LayoutManager.
   configure: function(opts) { 
     var options = Backbone.LayoutManager.prototype.options;
 
+    // Without this check the application would react strangely to a foreign
+    // input.
     if (_.isObject(opts)) {
-      Backbone.LayoutManager.prototype.options = _.extend({}, options, opts);
+      // Directly 
+      Backbone.LayoutManager.prototype.options = _.extend(options, opts);
     }
   }
 });
 
-// Defaults
+// Default configuration options; designed to be overriden.
+// Paths  : Template and layout properties containing paths to prefix for fetch.
+// Fetch  : Returns template contents as a string.
+// Partial: Injects template into the layout.
+// Render : Used to combine context objects to templates.
 Backbone.LayoutManager.prototype.options = {
-  engine: _.template,
+  paths: {},
 
-  // ASYNC fetch a template
+  // Fetch is passed a path and is expected to return template contents as a
+  // string.  As you can see below assigning this.async() to a variable
+  // can be used for asynchronous operations.
   fetch: function(path) {
     var done = this.async();
 
+    // Do a basic GET for the file path and call done.
     $.get(path, function(data) {
       done(data);
     });
   },
 
-  // Handling partials
+  // This is really the only way you will want to partially apply a view into
+  // a layout.  Its entirely possible you'll want to do it differently, so
+  // this method is available to change.
+  //
+  // @Layout   : Is the LayoutManager's el property.
+  // @Name     : Is the key name specified in the partial assignment.
+  // @Template : Is the View's el property.
   partial: function(layout, name, template) {
     $(layout).find(name).html(template);
   },
 
+  // By default render shouldn't do anything special.
   render: _.identity
 };
 

119e0a6e4259f3891fea942d5df39aaefc9072b6
tbranyen <tim@tabdeveloper.com>
2011-12-10 17:28:11 -0500

    updated version to 0.0.3

--- backbone.layoutmanager.js
+++ backbone.layoutmanager.js
@@ -1,4 +1,4 @@
-/* backbone.layoutmanager.js v0.0.2
+/* backbone.layoutmanager.js v0.0.3
  * Copyright 2011, Tim Branyen (@tbranyen)
  * backbone.layoutmanager.js may be freely distributed under the MIT license.
  */

4bea1c53688d3e57cb5e65907e8fe0fde5237fed
Amir Grozki <amirgrozki@gmail.com>
2012-05-20 10:53:37 +0300

    Fixed issue when appending views with view().

--- backbone.layoutmanager.js
+++ backbone.layoutmanager.js
@@ -325,6 +325,12 @@ var LayoutManager = Backbone.View.extend({
     // array.
     if (append) {
       partials = this.views[name] = this.views[name] || [];
+      
+      if ( !this.views[name].push ) {
+        // Ensure this.views[name] is an array.
+        partials = this.views[name] = [ this.views[name] ];
+      }
+      
       partials.push(view);
 
       return view;

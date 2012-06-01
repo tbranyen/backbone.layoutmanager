805a8124873c66b1be933e9a244e7d53983b2ecc
Tim Branyen <tim@tabdeveloper.com>
2012-06-01 13:28:59 -0700

    Merge pull request #83 from GeReV/master
    Issue #82: Exception when appending a view after setting a view

--- backbone.layoutmanager.js
+++ backbone.layoutmanager.js
@@ -179,6 +179,12 @@ var LayoutManager = Backbone.View.extend({
     // array.
     if (append) {
       partials = this.views[name] = this.views[name] || [];
+      
+      if (!_.isArray(this.views[name])) {
+        // Ensure this.views[name] is an array.
+        partials = this.views[name] = [this.views[name]];
+      }
+      
       partials.push(view);
 
       return view;

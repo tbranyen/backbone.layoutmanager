f67fa5de5e5e2877574a2cd967bee86d6a6993d3
Mike Pennisi <mike@mikepennisi.com>
2012-12-05 17:27:39 -0500

    Rename 'append' to 'insert'
    The name 'insert' is more generic: it does not imply the method by which
    elements are added to the DOM. Since this method is intended to be
    overridden with any arbitray insertion logic, the API should not suggest
    what form that method takes.

--- test/views.js
+++ test/views.js
@@ -1793,7 +1793,7 @@ test("Scoping nested view assignment selector to parent", 1, function() {
   equal(layout.$(".test div").html(), "lol", "Correct placeholder text");
 });
 
-test("'insertView' uses user-defined `append` method", 2, function() {
+test("'insertView' uses user-defined `insert` method", 2, function() {
   var hit = false;
   var layout = new Backbone.Layout({
     template: _.template("<div class='test'>World</div>"),
@@ -1803,7 +1803,7 @@ test("'insertView' uses user-defined `append` method", 2, function() {
   layout.insertView(".test", new Backbone.Layout({
       template: _.template("Hello"),
       fetch: _.identity,
-      append: function($root, child) {
+      insert: function($root, child) {
         $root.before(child);
         hit = true;
       }
@@ -1811,8 +1811,8 @@ test("'insertView' uses user-defined `append` method", 2, function() {
 
   layout.render();
 
-  ok(hit, "Invoked user-defined `append` method when rendering");
-  equal(layout.$el.text(), "HelloWorld", "Used user-defined `append` method to insert view HTML into layout");
+  ok(hit, "Invoked user-defined `insert` method when rendering");
+  equal(layout.$el.text(), "HelloWorld", "Used user-defined `insert` method to insert view HTML into layout");
 });
 
 // https://github.com/tbranyen/backbone.layoutmanager/issues/222

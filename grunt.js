6a10d56fde1b25e754855e378e1e729af351fc99
Mike Pennisi <mike@mikepennisi.com>
2012-12-18 16:27:59 -0500

    Add a grunt task for running tests in Node.js

--- grunt.js
+++ grunt.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
   "use strict";
 
+  grunt.loadTasks("tasks");
+
   grunt.initConfig({
     lint: {
       files: ["grunt.js", "backbone.layoutmanager.js"]
@@ -23,13 +25,26 @@ module.exports = function(grunt) {
       globals: {}
     },
 
+    "node-qunit": {
+      deps: ["./test/vendor/util.js"],
+      code: ".",
+      tests: [
+        "./test/expose.js",
+        "./test/configure.js",
+        "./test/setup.js",
+        "./test/views.js"
+      ]
+    },
+
     qunit: {
       underscore: ["test/underscore.html"],
       lodash: ["test/lodash.html"]
     }
   });
 
+  grunt.registerTask("test", "qunit:underscore qunit:lodash node-qunit");
+
   // Default task.
-  grunt.registerTask("default", "lint qunit:underscore qunit:lodash");
+  grunt.registerTask("default", "lint test");
 
 };

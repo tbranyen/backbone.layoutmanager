2921bad990230be35fc0cd35479df0c7b034a6a0
ssafejava <s@safejava.com>
2013-05-20 08:23:47 +0800

    remove newline

--- backbone.layoutmanager.js
+++ backbone.layoutmanager.js
@@ -227,7 +227,6 @@ var LayoutManager = Backbone.View.extend({
     _.each(views, function(view, name) {
       // If the view is an array put all views into insert mode.
       if (_.isArray(view)) {
-
         return _.each(view, function(view) {
           this.insertView(name, view);
         }, this);

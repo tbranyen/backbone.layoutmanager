6b7600e7a1584f57624ee9d475b92dc5c6e4511b
ssafejava <s@safejava.com>
2013-09-24 09:21:46 +0800

    Add a few more benches.

--- test/benchmark/spec/lists.js
+++ test/benchmark/spec/lists.js
@@ -1,4 +1,4 @@
-/*global Benchmark, Backbone, ui, document, _, $ */
+/*global Benchmark, Backbone, ui, _, $ */
 
 /**
  * Backbone.LayoutManager list rendering tests
@@ -16,12 +16,15 @@
   // Setup & teardown  
   Benchmark.prototype.setup = function(){
 
+    var tpl = "<div><h1><%= prop1 %></h1><%= prop2 %></div>";
+
     window.ListView = Backbone.Layout.extend({
       el: "#testArea",
       template: _.template("<div class=\"inner\"></div>"),
       initialize: function(options) {
+        var ItemView = options.noel ? window.ItemViewNoEl : window.ItemView;
         for (var i = 0; i < options.innerViews; i++){
-          this.insertView(".inner", new window.ItemView({
+          this.insertView(".inner", new ItemView({
             model: new Backbone.Model({
               prop1: Math.random() * 1000000,
               prop2: Math.random() * 1000000
@@ -32,9 +35,20 @@
     });
 
     window.ItemView = Backbone.Layout.extend({
-      template: _.template("<div><h1><%= prop1 %></h1><%= prop2 %></div>"),
+      template: _.template(tpl),
       serialize: function(){
         return this.model.toJSON();
+      }
+    });
+
+    window.ItemViewNoEl = window.ItemView.extend({
+      el: false
+    });
+
+    window.FakeListView = Backbone.Layout.extend({
+      el: "#testArea",
+      initialize: function(options) {
+        this.template = _.template(options.tpl);
       }
     });
 
@@ -48,45 +62,83 @@
   };
 
   // Add Benchmarks
+  // Examples:
+  // 
+  // Async:
+  // ui.add({
+  //   name: "Async Test",
+  //   defer: true,
+  //   fn: function(test) {
+  //     var list = new window.ListView({innerViews: 1000});
+  //     list.on("afterRender", function(){
+  //       // Call test.resolve() to end the test, binding is important
+  //       somethingAsync(test.resolve.bind(test));
+  //     });
+  //     list.render();
+  //   }
+  // })
+  // 
+  // Sync:
+  // ui.add({
+  //   name: "Sync test",
+  //   fn: function() {
+  //     var list = new window.ListView({innerViews: 1000});
+  //     list.render();
+  //   }
+  // })
+ 
   ui.add({
     name: "Create ListView with 1000 children",
-    defer: true,
     // Benchmark Body (timed section)
-    fn: function(test) {
+    fn: function() {
       var list = new window.ListView({innerViews: 1000});
-      list.on("afterRender", test.resolve, test);
       list.render();
     }
   })
   .add({
     name: "Create ListView with 1000 children (detached)",
-    defer: true,
-    // Benchmark Body
-    fn: function(test) {
+    fn: function() {
       var list = new window.ListView({innerViews: 1000});
       list.setElement($("<div>"));
-      list.on("afterRender", test.resolve, test);
       list.render();
     }
   })
   .add({
     name: "Create ListView with 10 children",
-    defer: true,
-    // Benchmark Body (timed section)
-    fn: function(test) {
+    fn: function() {
       var list = new window.ListView({innerViews: 10});
-      list.on("afterRender", test.resolve, test);
       list.render();
     }
   })
   .add({
     name: "Create ListView with 10 children (detached)",
-    defer: true,
-    // Benchmark Body
-    fn: function(test) {
+    fn: function() {
       var list = new window.ListView({innerViews: 10});
       list.setElement($("<div>"));
-      list.on("afterRender", test.resolve, test);
+      list.render();
+    }
+  })
+  .add({
+    name: "Create ListView with 10 children (noel)",
+    fn: function() {
+      var list = new window.ListView({innerViews: 10, noel: true});
+      list.render();
+    }
+  })
+  .add({
+    name: "Fake list view with 1000 children (testing batch html parsing)",
+    setup: function() {
+      var tpl = "<div><h1><%= prop1 %></h1><%= prop2 %></div>";
+      var longTpl = "";
+      for (var i = 0; i < 1000; i++) {
+        longTpl += tpl;
+      }
+      window.FakeListView = window.ListView.extend({
+        template: _.template(longTpl)
+      });
+    },
+    fn: function() {
+      var list = new window.FakeListView();
       list.render();
     }
   });
